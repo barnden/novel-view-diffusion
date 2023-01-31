e4b9cb2e4030f97a010c4752296d88982f7b5247
barnden <brandon@nguyen.vc>
2023-01-31 13:21:44 -0600

    Squeeze ray tensors

--- Camera.py
+++ Camera.py
@@ -4,13 +4,16 @@ import torch.nn.functional as F
 
 class Camera:
     def __init__(self, H, W, K, R, t):
-        self.Kinv = torch.inverse(K)
         self.padding = 6 - R.ndim
 
         self.H = H
         self.W = W
+
+        Kinv = torch.inverse(K)
+
         self.R = R.reshape(R.shape[:-2] + (1,) * self.padding + R.shape[-2:])
         self.t = t.reshape(t.shape[:-1] + (1,) * self.padding + t.shape[-1:])
+        self.Kinv = Kinv.reshape(Kinv.shape[:-2] + (1,) * self.padding + Kinv.shape[-2:])
 
     def centers(self):
         h, w = torch.meshgrid(torch.arange(self.H), torch.arange(self.W), indexing="xy")
@@ -30,5 +33,8 @@ class Camera:
         dirs = F.normalize(dirs, dim=-1)
 
         pos = self.t.repeat(1, 1, self.H, self.W, 1)
+
+        pos = pos.squeeze()
+        dirs = dirs.squeeze()
 
         return (pos, dirs)
